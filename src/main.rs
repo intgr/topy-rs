66418922649c7053cb16f51fbb21745a8086a7ae
Marti Raudsepp <marti@juffo.org>
2021-07-01 23:35:53 +0300

    Write replacements back to the original file
    This currently uses a fork of `fancy-regex` that implements replace methods.
    
    TODO: Overwrite shouldn't be the default behavior, but is better than nothing.
    
    TODO: Many replacement substitutions are not handled correctly yet.

--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,13 @@
 use crate::tagsoup::{parse_and_compile, TypoRule};
-use fancy_regex::Regex;
 use log::{debug, error, info};
 use rayon::prelude::*;
+use std::borrow::Cow;
 use std::env::args_os;
 use std::error::Error;
 use std::ffi::OsString;
-use std::fs::File;
+use std::fs::{File, OpenOptions};
 use std::io::Read;
+use std::io::Write;
 
 mod logging;
 mod tagsoup;
@@ -30,21 +31,6 @@ fn apply_files(rules: &[TypoRule], paths: Vec<OsString>) -> u32 {
         .sum()
 }
 
-fn regex_replace(input: String, regex: &Regex, replace: &str) -> (String, u32) {
-    let content = input.clone();
-    let mut count = 0u32;
-
-    for mat in regex.find_iter(&input.as_str()) {
-        let mat = mat.unwrap();
-        // FIXME implement capture groups in replace
-        // FIXME need to rejigger the ranges of successive matches
-        // content.replace_range(mat.range(), replace);
-        debug!("{} -> {}", mat.as_str(), replace);
-        count += 1;
-    }
-    (content, count)
-}
-
 fn apply_file_inner(rules: &[TypoRule], path: &OsString) -> Result<u32, AnyErr> {
     let mut file = File::open(path)?;
     let mut contents = String::new();
@@ -52,18 +38,30 @@ fn apply_file_inner(rules: &[TypoRule], path: &OsString) -> Result<u32, AnyErr> 
     file.read_to_string(&mut contents)?;
 
     for rule in rules {
-        let result = regex_replace(contents, &rule.regex, &rule.replace);
-        contents = result.0;
-        let count = result.1;
-        total_count += count;
-        if count > 0 {
-            info!(
-                "{}: Applied {} x '{}'",
+        let result = rule.regex.replace_all(&contents, &rule.replace);
+        // Borrowed value was returned -- no changes were made, ignore.
+        if let Cow::Borrowed(_) = result {
+            continue;
+        }
+        if contents == result {
+            debug!(
+                "{}: '{}' had a no-op match",
                 path.to_string_lossy(),
-                count,
                 rule.label
             );
+            continue;
         }
+
+        info!("{}: Applied '{}'", path.to_string_lossy(), rule.label);
+        total_count += 1;
+        contents = result.into();
+    }
+
+    // TODO: Overwriting files shouldn't be the default behavior
+    if total_count > 0 {
+        let mut file = OpenOptions::new().write(true).truncate(true).open(path)?;
+        file.write_all(contents.as_bytes())?;
+        file.flush()?;
     }
     Ok(total_count)
 }
